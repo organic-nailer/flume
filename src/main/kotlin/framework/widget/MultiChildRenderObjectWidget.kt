321023e38beb32012bb051674a84a00ffe9c5a54
organic-nailer <hykwyuk0125@gmail.com>
2022-07-30 17:25:28 +0900

    MultiChildRenderObjectElement.ktを移動

--- src/main/kotlin/framework/widget/MultiChildRenderObjectWidget.kt
+++ src/main/kotlin/framework/widget/MultiChildRenderObjectWidget.kt
@@ -1,7 +1,7 @@
 package framework.widget
 
 import framework.element.Element
-import framework.geometrics.MultiChildRenderObjectElement
+import framework.element.MultiChildRenderObjectElement
 
 abstract class MultiChildRenderObjectWidget(
     val children: List<Widget>,
