d0b2937991476541b657b5b12b4c66b66fe84f73
organic-nailer <hykwyuk0125@gmail.com>
2022-08-21 15:56:35 +0900

    ダイエット

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -33,9 +33,6 @@ fun appMain() {
             "g" -> runApp(createWidgetTree(LightPhase.Green))
             "a" -> runApp(createWidgetTree(LightPhase.All))
         }
-        WidgetsFlumeBinding.pipeline.renderView!!.needsLayout = true
-        WidgetsFlumeBinding.pipeline.nodesNeedingLayout.add(WidgetsFlumeBinding.pipeline.renderView!!)
-        WidgetsFlumeBinding.ensureVisualUpdate()
     }
 }
 
@@ -53,75 +50,4 @@ fun createWidgetTree(phase: LightPhase = LightPhase.All): Widget {
             )
         )
     )
-//    return Align(
-//        child = Flex(
-//            mainAxisSize = MainAxisSize.Min,
-//            direction = Axis.Vertical,
-//            children = listOf(
-//                ClipPath(
-//                    clipper = ArcClipper(),
-//                    child = SizedBox(
-//                        width = 100.0, height = 100.0,
-//                        child = ColoredBox(
-//                            color = if(phase in listOf(LightPhase.Red, LightPhase.All)) 0xFFF44336.toInt()
-//                                    else darken
-//                        )
-//                    )
-//                ),
-//                ClipRRect(
-//                    borderRadius = BorderRadius.circular(20.0),
-//                    child = SizedBox(
-//                        width = 100.0, height = 100.0,
-//                        child = ColoredBox(
-//                            color = if(phase in listOf(LightPhase.Yellow, LightPhase.All)) 0xFFFFEB3B.toInt()
-//                            else darken
-//                        )
-//                    )
-//                ),
-//                ClipOval(
-//                    child = SizedBox(
-//                        width = 100.0, height = 100.0,
-//                        child = ColoredBox(
-//                            color = if(phase in listOf(LightPhase.Green, LightPhase.All)) 0xFF4CAF50.toInt()
-//                            else darken
-//                        )
-//                    )
-//                ),
-//                RichText(
-//                    TextSpan(
-//                        "信号機だよ",
-//                        textStyle = TextStyle().apply {
-//                            color = 0xFFFF0000.toInt()
-//                            fontSize = 50f
-//                        }
-//                    )
-//                )
-//            )
-//        )
-//    )
-}
-
-class ArcClipper: CustomClipper<Path>() {
-    override fun getClip(size: Size): Path {
-        return Path().apply {
-            lineTo(0f, size.height.toFloat() - 30f)
-
-            val firstControlPoint = Offset(size.width / 4, size.height)
-            val firstPoint = Offset(size.width / 2, size.height)
-            quadTo(firstControlPoint.dx.toFloat(),
-                firstControlPoint.dy.toFloat(),
-                firstPoint.dx.toFloat(),
-                firstPoint.dy.toFloat())
-
-            val secondControlPoint = Offset(size.width - size.width / 4, size.height)
-            val secondPoint = Offset(size.width, size.height - 30)
-            quadTo(secondControlPoint.dx.toFloat(),
-                secondControlPoint.dy.toFloat(),
-                secondPoint.dx.toFloat(),
-                secondPoint.dy.toFloat())
-            lineTo(size.width.toFloat(), 0f)
-            closePath()
-        }
-    }
-    override fun shouldReclip(oldClipper: CustomClipper<Path>): Boolean = false
 }
