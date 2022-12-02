9e0f8ee6d36eb9b0e509e0095f016a79f952fa02
organic-nailer <hykwyuk0125@gmail.com>
2022-12-02 11:43:55 +0900

    ColoredBoxで子をペイントする2

--- src/main/kotlin/framework/render/RenderColoredBox.kt
+++ src/main/kotlin/framework/render/RenderColoredBox.kt
@@ -9,6 +9,8 @@ class RenderColoredBox(val color: Int) : RenderProxyBox() {
         if (size.width != 0.0 && size.height != 0.0) {
             context.canvas.drawRect(size.and(offset), Paint().also { it.color = color })
         }
-        child?.paint(context, offset)
+        child?.let {
+            context.paintChild(it, offset)
+        }
     }
 }
